7e299d6f2cc1d52f281feeab8af2e9e8d43fbdc7
xxmonster <x13133053566@163.com>
2017-03-03 11:35:35 +0800

    添加自定义日历的功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,8 @@ var common = require('../../utils/util.js');
 var page_app = getApp();
 Page({
   data: {
+    hasEmptyGrid: false,
+    dayHide: true,
     TaskItems: wx.getStorageSync('tasksData') || [],
     // TaskItems: [//内容列表
       // {value: '内容1', finshed: true, id : 0},
@@ -175,5 +177,130 @@ Page({
       newTask: ""
     })
     wx.setStorageSync('tasksData', AllDatas);
+  },
+  showSelectDate(){//显示选择日期
+    const date = new Date();
+    const cur_year = date.getFullYear();
+    const cur_month = date.getMonth() + 1;
+    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+    this.calculateEmptyGrids(cur_year, cur_month);
+    this.calculateDays(cur_year, cur_month);
+    this.getSystemInfo();
+    this.setData({
+      cur_year,
+      cur_month,
+      weeks_ch,
+      dayHide: false
+    })
+  },
+  hideSelectDate(){//隐藏选择日期
+    this.setData({
+      dayHide: true
+    })
+  },
+  selectDay(ev){//选择指定日期
+    console.log(ev)
+  },
+
+
+
+  getSystemInfo() {
+    try {
+      const res = wx.getSystemInfoSync();
+      this.setData({
+        scrollViewHeight: res.windowHeight * res.pixelRatio || 667
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  getThisMonthDays(year, month) {
+    console.log(year)
+    return new Date(year, month, 0).getDate();
+  },
+  getFirstDayOfWeek(year, month) {
+    return new Date(Date.UTC(year, month - 1, 1)).getDay();
+  },
+  calculateEmptyGrids(year, month) {
+    const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+    let empytGrids = [];
+    if (firstDayOfWeek > 0) {
+      for (let i = 0; i < firstDayOfWeek; i++) {
+        empytGrids.push(i);
+      }
+      this.setData({
+        hasEmptyGrid: true,
+        empytGrids
+      });
+    } else {
+      this.setData({
+        hasEmptyGrid: false,
+        empytGrids: []
+      });
+    }
+  },
+  calculateDays(year, month) {
+    let days = [];
+
+    const thisMonthDays = this.getThisMonthDays(year, month);
+
+    for (let i = 1; i <= thisMonthDays; i++) {
+      days.push(i);
+    }
+
+    this.setData({
+      days
+    });
+  },
+  // onLoad(options) {
+  //   const date = new Date();
+  //   const cur_year = date.getFullYear();
+  //   const cur_month = date.getMonth() + 1;
+  //   const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+  //   this.calculateEmptyGrids(cur_year, cur_month);
+  //   this.calculateDays(cur_year, cur_month);
+  //   this.getSystemInfo();
+  //   this.setData({
+  //     cur_year,
+  //     cur_month,
+  //     weeks_ch
+  //   })
+  // },
+  handleCalendar(e) {
+    const handle = e.currentTarget.dataset.handle;
+    const cur_year = this.data.cur_year;
+    const cur_month = this.data.cur_month;
+    if (handle === 'prev') {
+      let newMonth = cur_month - 1;
+      let newYear = cur_year;
+      if (newMonth < 1) {
+        newYear = cur_year - 1;
+        newMonth = 12;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+
+    } else {
+      let newMonth = cur_month + 1;
+      let newYear = cur_year;
+      if (newMonth > 12) {
+        newYear = cur_year + 1;
+        newMonth = 1;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+    }
   }
 })
