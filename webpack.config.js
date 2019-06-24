d1c94e144b4e96a5e1fd6b93f9de844dde4c9dc4
jusetwe! <huaxi.li@renren-inc.com>
2019-06-24 14:29:58 +0800

    完成基本的scss编译

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,16 @@
 const path = require("path");
-const glob = require("glob");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const globby = require("globby");
+const CopyWebpackPlugin = require("copy-webpack-plugin"); // 复制文件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 单独打包样式文件
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries"); //删除多余js
 
-// 单独打包样式文件
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//删除多余js
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-
-function resolve(dir) {
-  return path.join(__dirname, "..", dir);
-}
-
-const CSS_PATH = {
-  pattern: ["./src/pages/**/*.scss"],
-  src: path.join(__dirname, "src")
-};
-
-// 遍历所有需要打包的SCSS/CSS文件路径
-/* let getCSSEntries = (config) => {
-	let fileList = glob.sync(config.pattern);
-	return fileList.reduce((previous, current) => {
-		let filePath = path.parse(path.relative(config.src, current));
-		let withoutSuffix = path.join(filePath.dir, filePath.name);
-		previous[withoutSuffix] = path.resolve(__dirname, current);
-		return previous;
-	}, {});
-}; */
-
-// console.log(getCSSEntries(CSS_PATH));
-
-// getEntry()
-// function getEntry (rootSrc) {
-//   var map = {};
-//   glob.sync('./src/pages/**/*.scss')
-//   .forEach(file => {
-//     console.log(file);
-//     /* var key = relative(rootSrc, file).replace('.js', '');
-//     map[key] = file; */
-//   })
-//    return map;
-// }
-
-function recursiveIssuer(m) {
-  if (m.issuer) {
-    return recursiveIssuer(m.issuer);
-  } else if (m.name) {
-    return m.name;
-  } else {
-    return false;
-  }
-}
+const ENTRY_PATH = ["./src/*.scss", "./src/pages/**/**/*.scss"];
 
 module.exports = {
-  mode: "production",
-  entry: ["./src/pages/detail/detail.scss", "./src/pages/index/index.scss"],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        fooStyles: {
-          name: 'detail',
-          test: (m, c, entry = 'detail') =>
-            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
-          chunks: 'all',
-          enforce: true,
-        },
-        barStyles: {
-          name: 'index',
-          test: (m, c, entry = 'index') =>
-            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
-  },
+  entry: getEntries(ENTRY_PATH),
+  // entry: ["./src/pages/detail/detail.scss", "./src/pages/index/index.scss"],
   output: {
-    path: '/dist',
-    filename: '[name].css'
+    path: path.join(__dirname, "/dist")
   },
   module: {
     rules: [
@@ -84,33 +18,62 @@ module.exports = {
         test: /\.(scss|sass|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          // 需要用到的 loader
           "css-loader",
-          "sass-loader"
+          "sass-loader",
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: [resolveResouce("mixin.scss")] // it need a absolute path
+            }
+          }
         ]
       }
     ]
   },
   plugins: [
+    /* 删除多余的js入口信息 */
     new FixStyleOnlyEntriesPlugin(),
-    // new MiniCssExtractPlugin({
-    // 	filename: '/src/pages/**/[name].wxss'
-    // }),
+    /* 处理样式表后缀名 */
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].wxss",
-      chunkFilename: "[id].wxss"
-    })
-    // new CopyWebpackPlugin(
-    //   [
-    //     {
-    //       from: "**/*",
-    //       to: "",
-    //       ignore: ['*.scss'],
-    //       context: "src/"
-    //     }
-    //   ]
-    // )
+      filename: "[name].wxss"
+    }),
+    /* 复制其余所有未处理文件至指定的小程序工程目录 */
+    new CopyWebpackPlugin([
+      {
+        from: "**/*",
+        to: "",
+        ignore: ["*.scss"],
+        context: "src/"
+      }
+    ])
   ]
 };
+
+/**
+ * 混入的样式位置
+ * @author huaxi.li
+ * @date 2019-06-24
+ * @param {*} name scss文件的名称
+ * @returns
+ */
+function resolveResouce(name) {
+  return path.resolve(__dirname, "./src/common/style/" + name);
+}
+/**
+ * 根据入口列表获取入口文件对象格式
+ * @author huaxi.li
+ * @date 2019-06-24
+ * @param {*} pattern Array[String]
+ * @returns
+ */
+function getEntries(pattern) {
+  let fileList = globby.sync(pattern); //读出文件列表
+  return fileList.reduce((entryObj, current) => {
+    let filePath = path.parse(
+      path.relative(path.join(__dirname, "src"), current)
+    ); //读取基于当前文件系统路径信息
+    let filePathUri = path.join(filePath.dir, filePath.name); //合并出webpack entry的 key
+    entryObj[filePathUri] = path.resolve(__dirname, current);
+    return entryObj;
+  }, {});
+}
---
+++ webpack.development.config.js
@@ -0,0 +1,7 @@
+const config = require("./webpack.config");
+const merge = require("webpack-merge");
+
+module.exports = merge(config, {
+  mode: 'development',
+  watch: true
+});
